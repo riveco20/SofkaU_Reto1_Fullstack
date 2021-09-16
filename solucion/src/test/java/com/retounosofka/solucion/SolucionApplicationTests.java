f78845f1f45362823a0053e291835128cd8ee648
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-09-16 07:46:26 -0500

    Agregando front y back del reto uno sofka

---
+++ solucion/src/test/java/com/retounosofka/solucion/SolucionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retounosofka.solucion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolucionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
